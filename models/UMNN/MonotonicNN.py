6312c97ccd32e417dc6da6a08855bf0da4d2371b
awehenkel <antoine.wehenkel@gmail.com>
2020-11-17 13:47:35 +0100

    Add multiple output for monotonic NN

--- models/UMNN/MonotonicNN.py
+++ models/UMNN/MonotonicNN.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-from .NeuralIntegral import NeuralIntegral
-from .ParallelNeuralIntegral import ParallelNeuralIntegral
+from NeuralIntegral import NeuralIntegral
+from ParallelNeuralIntegral import ParallelNeuralIntegral
 
 
 def _flatten(sequence):
@@ -10,10 +10,10 @@ def _flatten(sequence):
 
 
 class IntegrandNN(nn.Module):
-    def __init__(self, in_d, hidden_layers):
+    def __init__(self, in_d, hidden_layers, n_out=1):
         super(IntegrandNN, self).__init__()
         self.net = []
-        hs = [in_d] + hidden_layers + [1]
+        hs = [in_d] + hidden_layers + [n_out]
         for h0, h1 in zip(hs, hs[1:]):
             self.net.extend([
                 nn.Linear(h0, h1),
@@ -28,11 +28,17 @@ class IntegrandNN(nn.Module):
 
 
 class MonotonicNN(nn.Module):
-    def __init__(self, in_d, hidden_layers, nb_steps=50, dev="cpu"):
+    '''
+    in_d : The total number of inputs
+    hidden_layers : a list a the number of neurons, to be used by a network that compresses the non-monotonic variables and by the integrand net.
+    nb_steps : Number of integration steps
+    n_out : the number of output (each output will be monotonic w.r.t one variable)
+    '''
+    def __init__(self, in_d, hidden_layers, nb_steps=50, n_out=1, dev="cpu"):
         super(MonotonicNN, self).__init__()
-        self.integrand = IntegrandNN(in_d, hidden_layers)
+        self.integrand = IntegrandNN(in_d, hidden_layers, n_out)
         self.net = []
-        hs = [in_d-1] + hidden_layers + [2]
+        hs = [in_d-1] + hidden_layers + [2 * n_out]
         for h0, h1 in zip(hs, hs[1:]):
             self.net.extend([
                 nn.Linear(h0, h1),
@@ -43,6 +49,7 @@ class MonotonicNN(nn.Module):
         self.net = nn.Sequential(*self.net)
         self.device = dev
         self.nb_steps = nb_steps
+        self.n_out = n_out
 
     '''
     The forward procedure takes as input x which is the variable for which the integration must be made, h are just other conditionning variables.
@@ -50,8 +57,8 @@ class MonotonicNN(nn.Module):
     def forward(self, x, h):
         x0 = torch.zeros(x.shape).to(self.device)
         out = self.net(h)
-        offset = out[:, [0]]
-        scaling = torch.exp(out[:, [1]])
+        offset = out[:, :self.n_out]
+        scaling = torch.exp(out[:, self.n_out:])
         return scaling*ParallelNeuralIntegral.apply(x0, x, self.integrand, _flatten(self.integrand.parameters()), h, self.nb_steps) + offset
 
     '''
@@ -59,7 +66,7 @@ class MonotonicNN(nn.Module):
     '''
     def inverse(self, y, h):
         out = self.net(h)
-        y0 = out[:, [0]]
-        scaling = torch.exp(out[:, [1]])
+        y0 = out[:, :self.n_out]
+        scaling = torch.exp(out[:, self.n_out:])
         return ParallelNeuralIntegral.apply(y0, y, self.integrand, _flatten(self.integrand.parameters()), h,
                                             self.nb_steps)/scaling
--- models/setup.py
+++ models/setup.py
@@ -3,10 +3,10 @@ import setuptools
 
 setup(
     name='UMNN',
-    version='1.5',
+    version='1.6',
     author="Antoine Wehenkel",
     author_email="antoine.wehenkel@gmail.com",
-    description="Implementation of unconstrained monotonic neural networks",
+    description="Implementation of unconstrained monotonic neural networks and their application for autoregressive normalizing flows.",
     long_description='',
     long_description_content_type="text/markdown",
     url="https://github.com/awehenkel/umnn",
